93a43d5d8dd0f07ae224f688971819fb6bd00e40
sergiolooky <111609774+sergiolooky@users.noreply.github.com>
2022-08-31 23:00:46 -0400

    Add files via upload

---
+++ aprobacionyreprobados1.py
@@ -0,0 +1,16 @@
+# APROBACIONYREPROBACION
+nota1=int(input("ingrese primera nota"))
+nota2=int(input("ingrese segunda nota"))
+nota3=int(input("ingrese tercera nota"))
+promedio = (nota1 + nota2 + nota3)/3
+if promedio > 50:
+    aprobado = promedio
+else:
+    reprobado = promedio
+print("reprobado",promedio)
+
+           
+
+
+
+
